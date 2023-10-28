49f14fdc1912c2f029e5dbb73f409560f7977f5a
sheeshagar-iqbal <iqbalkhan234786@gmail.com>
2023-10-28 06:18:45 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// var a=0
+// while (a<10) { console.log("sorry"+a)
+//     a++
+// }
+// var a
+// var b
+// function sqrt(a,b) {
+//     console.log(a*a+ b*b)
+// }
+// sqrt(10,20)``
+// var arr[12, 'array',23,44,]
+// console.log(arr[arr.length-1])44
+
+     var arr= []
+     for( var a=0; a<10; a++){
+        arr.push(a)
+        
+     }
+   console.log(arr)
